34ba11bed38bbf243b3b7753e31509f95fe370a1
Cory Bell <corybell21@gmail.com>
2021-03-20 09:03:23 -0500

    clean up

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import "@fontsource/montserrat/900.css"
 import '@fontsource/montserrat'
 
 // import '@fontsource/rubik'
-import '@fontsource/merriweather'
+// import '@fontsource/merriweather'
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
