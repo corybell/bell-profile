d772378df63a09433e38662560428e8d4984fbaa
Cory Bell <corybell21@gmail.com>
2021-03-24 16:44:56 -0500

    remove unused packages, files

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,8 +1,5 @@
 import "@fontsource/montserrat/900.css"
 import '@fontsource/montserrat'
 
-// import '@fontsource/rubik'
-// import '@fontsource/merriweather'
-
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
